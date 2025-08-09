ce22ddab1937aa72daf49b2b1cd8d21cd0ecb1e8
mabry <58089454+mabryerik@users.noreply.github.com>
2025-08-09 12:33:58 -0500

    Update _data/metadata.js (via Pages CMS)

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "rip",
+	url: "https://mabry.rip/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "he dead",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "mabry",
+		email: "ok@mabry.rip",
+		url: "https://mabry.rip/at"
 	}
 }
